02cb85feb92de87171efa939692761d06f5eb22f
AABelimov <belimov98@gmail.com>
2023-11-10 15:47:32 +0300

    Added entity and create table

---
+++ src/main/java/pro/sky/telegrambot/entity/NotificationTask.java
@@ -0,0 +1,45 @@
+package pro.sky.telegrambot.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notification_tasks")
+public class NotificationTask {
+
+    @Id
+    private Long id;
+
+    @Column(name = "text", length = 4096, nullable = false)
+    private String text;
+
+    @Column(name = "date_time", nullable = false)
+    private LocalDateTime dateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
